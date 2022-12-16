ea087287f86cda49070d6e30645a7af7a70f5d4a
nic <nrhoda14@gmail.com>
2022-12-16 00:26:07 -0600

    deleted Develop directory to fix routing issues, started post routing

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const { json } = require('express');
+
 const express = require('express');
-const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const jsonNotes = require('./Develop/db/db.json');
+const jsonNotes = require('./db/db.json');
 const path = require('path');
+const { json } = require('express');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('./'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 app.get('/notes', (req, res) =>  {
-    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
     res.json(jsonNotes);
 });
+
+app.post('/api/notes', (req, res) => {
+    
+});
+
 
 
 
